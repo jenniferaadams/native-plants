8dfe30244e8240275775f77121bd1310e33d5b66
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-05-24 22:27:21 -0400

    created project

---
+++ backend/src/main/java/com/JA/nativeplants/NativeplantsApplication.java
@@ -0,0 +1,13 @@
+package com.JA.nativeplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeplantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeplantsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/JA/nativeplants/NativeplantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JA.nativeplants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeplantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
